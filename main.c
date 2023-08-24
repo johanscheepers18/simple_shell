1f593338336b7e4d75faa2962b361bfd3ed23c78
Johan Scheepers <johanscheepers718@gmail.com>
2023-08-24 13:26:46 +0200

    add comments to main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "shell.h"
 
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
